d36a524f507d2a023cc0524a022e6acf2086632c
denn-p <dennielysp@gmail.com>
2025-10-29 04:28:18 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandom (array){
+  let randomIndex = Math.floor(Math.random() * array.length);
+  let element = array [randomIndex];
+  return element
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getExcuse() {
+  let randomWho = getRandom(who);
+  let randomAction = getRandom(action);
+  let randomWhat = getRandom(what);
+  let randomWhen = getRandom(when);
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  document.getElementById('excuse').innerHTML = excuse;
+  return excuse;
+}
